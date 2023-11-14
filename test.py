a47f71fb231b26a4683f4885645c62712b41f428
Adrian <reinieradrianluna@gmail.com>
2023-11-14 16:05:01 +0800

    dropdown list initial

--- test.py
+++ test.py
@@ -1,119 +1,55 @@
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty
 from kivymd.app import MDApp
-from kivymd.uix.screen import Screen
-from kivymd.uix.list import MDList, TwoLineListItem
-from kivymd.uix.dialog import MDDialog
-from kivymd.uix.button import MDFlatButton
-from kivy.uix.scrollview import ScrollView
+from kivy.lang import Builder
+from kivymd.uix.button import MDRaisedButton
 from kivymd.uix.menu import MDDropdownMenu
-import mysql.connector
+from kivy.properties import ObjectProperty
 
 KV = '''
-Screen:
-    MDFlatButton:
+BoxLayout:
+    orientation: 'vertical'
+    MDRaisedButton:
         id: button
-        text: 'Open Menu'
-        pos_hint: {'center_x': 0.5, 'center_y': 0.5}
-        on_release: app.show_incident_type_dropdown(button)
+        text: "Report Incident"
+        pos_hint: {"center_x": 0.5, "center_y": 0.5}
+        on_release: app.menu_callback()
 '''
 
-# Database configuration
-host = "112.198.173.169"
-user = "root"
-password = "incidentreportingapp"
-database = "reportingApp"
+class MainApp(MDApp):
+    dropdown = ObjectProperty()
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="incidentreportingapp",
-    database="reportingApp"
-)
-cursor = db.cursor()
-
-class DropDownHandler(BoxLayout):
-    selected_item = StringProperty('')  # Property to store the selected item
-
-    def __init__(self, caller, **kwargs):
-        super().__init__(**kwargs)
-        self.orientation = 'vertical'
-        self.caller = caller
-        self.create_dropdown_menu()
-
-    def create_dropdown_menu(self):
-        menu_items = [{"viewclass": "OneLineListItem",
-                       "text": option,
-                       "on_release": lambda x=option: self.menu_callback(x)} 
-                      for option in ["Preparing to deploy", "On the Process", "Resolved"]]
-        self.dropdown_menu = MDDropdownMenu(
-            caller=self.caller,
-            items=menu_items,
-            position="center",
-            width_mult=4
-        )
-        self.dropdown_menu.open()
-
-    def menu_callback(self, text_item):
-        self.selected_item = text_item
-        self.dropdown_menu.dismiss()
-
-class MyApp(MDApp):
     def build(self):
-        self.theme_cls.primary_palette = "Green"
-        self.screen = Screen()
-        Builder.load_string(KV)
+        self.theme_cls.primary_palette = "Blue"
+        return Builder.load_string(KV)
+    
+    def on_start(self):
+        # Menu items list
+        menu_items = [
+            {"viewclass": "OneLineListItem", "text": "Medical Emergency"},
+            {"viewclass": "OneLineListItem", "text": "Natural Disaster"},
+            {"viewclass": "OneLineListItem", "text": "Security Threat"},
+            {"viewclass": "OneLineListItem", "text": "Others"}
+        ]
+        
+        # Create the dropdown menu
+        self.dropdown = MDDropdownMenu(
+            caller=self.root.ids.button,
+            items=menu_items,
+            width_mult=4,
+            position="center"
+        )
+        
+        # Set the release callback for each menu item
+        for item in menu_items:
+            item['on_release'] = lambda x=item['text']: self.option_callback(x)
 
-        scroll = ScrollView()
-        self.list_view = MDList()
-        scroll.add_widget(self.list_view)
-        self.screen.add_widget(scroll)
-        self.populate_list()
+    def menu_callback(self):
+        # Open the dropdown menu
+        self.dropdown.open()
 
-        return self.screen
+    def option_callback(self, option_text):
+        # Print the text of the selected option
+        print(option_text)
+        # Close the dropdown to avoid double opening on multiple clicks
+        self.dropdown.dismiss()
 
-    def populate_list(self):
-        cursor.execute("SELECT ReportId, Title FROM report")
-        rows = cursor.fetchall()
-
-        for row in rows:
-            item = TwoLineListItem(
-                text='Report Id: ' + str(row[0]),
-                secondary_text='Title: ' + row[1],
-                on_release=lambda x, row=row: self.open_dialog(row)
-            )
-            self.list_view.add_widget(item)
-
-    def open_dialog(self, row):
-        self.selected_report_id = row[0]
-        self.dropdown_handler = DropDownHandler(self.screen)  # Store the reference to dropdown_handler
-        content = BoxLayout(orientation='vertical')
-        content.add_widget(self.dropdown_handler)
-
-        self.dialog = MDDialog(title="Update Status",
-                               type="custom",
-                               content_cls=content,
-                               size_hint=(0.7, 0),
-                               buttons=[
-                                   MDFlatButton(
-                                       text="Submit",
-                                       theme_text_color="Custom",
-                                       text_color=self.theme_cls.primary_color,
-                                       on_release=self.submit_data
-                                   ),
-                               ])
-        self.dialog.open()
-
-    def show_incident_type_dropdown(self, caller):
-        dropdown_handler = DropDownHandler(caller)
-        dropdown_handler.create_dropdown_menu()
-
-    def submit_data(self, instance):
-        new_status = self.dropdown_handler.selected_item  # Use the stored reference
-        print('Selected Status: ' + new_status)
-        cursor.execute("UPDATE report SET status = %s WHERE ReportId = %s", (new_status, self.selected_report_id))
-        db.commit()
-        self.dialog.dismiss()
-
-MyApp().run()
+MainApp().run()
